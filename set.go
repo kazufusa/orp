30c5027aa7825b3e46555f2d17062a8e1239ea8e
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2022-07-16 00:29:21 +0900

    fix(fix bug):

--- set.go
+++ set.go
@@ -3,16 +3,17 @@ package orp
 type Set = []string
 
 func NewSet(ss []string) Set {
+	ret := make(Set, 0, len(ss))
 	m := make(map[string]struct{}, len(ss))
 	for _, s := range ss {
 		if s == "" {
 			continue
 		}
+		if _, ok := m[s]; ok {
+			continue
+		}
+		ret = append(ret, s)
 		m[s] = struct{}{}
-	}
-	ret := make(Set, 0, len(m))
-	for key := range m {
-		ret = append(ret, key)
 	}
 	return ret
 }
