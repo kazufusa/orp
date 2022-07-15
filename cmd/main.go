c8e8005e6e9b0480350ad39fd66bd9bb1c8757df
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2022-07-15 22:41:53 +0900

    feat(cmd): add main

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kazufusa/orp"
+)
+
+func main() {
+	if isHelpNeeded() {
+		help()
+		return
+	}
+
+	env := orp.NewEnv("PATH", ":")
+	for i := len(os.Args) - 1; i >= 0; i-- {
+		env.MoveToTop(os.Args[i])
+	}
+	fmt.Printf(env.Export())
+}
+
+func isHelpNeeded() bool {
+	for _, arg := range os.Args {
+		if arg == "--help" || arg == "-h" || arg == "/?" {
+			return true
+		}
+	}
+	return false
+}
+
+func help() {
+	fmt.Printf(`orp can arrange the order of  PATH environment variable
+
+Usage:
+    orp [regexp of high-priority dir]...
+`)
+}
